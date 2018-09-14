92618704a78c98b5fb24e83846d471a6fa2f83ee
JoeyDelahoussaye <jldela@gmail.com>
2018-09-14 17:01:16 -0500

    added login page

--- client/utilities.js
+++ client/utilities.js
@@ -3,6 +3,7 @@ const {
   googleApi,
 } = require('./config');
 
+
 function addNewUser(email, password) {
   if (email === undefined && password === undefined) {
     console.log('made call to server which added to database');
@@ -12,7 +13,18 @@ function addNewUser(email, password) {
     });
     console.log(email, password);
   }
-}
+} 
+
+function checkUser(email, password) {
+  if (email === undefined && password === undefined) {
+    console.log('made call to server which added to database');
+  } else {
+    axios.get('/home').then((resolve) => {
+      console.log(resolve);
+    });
+    console.log(email, password);
+  }
+} 
 
 function getCoordinates(address) {
   if (address === undefined) {
@@ -23,39 +35,11 @@ function getCoordinates(address) {
   }
 }
 
-// var settings = {
-    //   "async": true,
-    //   "crossDomain": true,
-    //   "url": "https://maps.googleapis.com/maps/api/directions/json?origin=4707+eilers+avenue+austin+texas&destination=Universal+Studios+Hollywood&key=AIzaSyDyOcw4O6ZqUChULjprwYUoa33GHO5I7AE",
-    //   "method": "GET",
-    //   "headers": {
-    //     "Content-Type": "application/json",
-    //     "Cache-Control": "no-cache",
-    //     "Postman-Token": "a2cf149f-fe37-495d-ad31-69ec24747bf0"
-    //   }
-    // }
-
-function getDriveTime(address1, address2) {
-  if (address1 === undefined) {
-    console.log('made call to server which added to database');
-  } else {
-    const formattedAddress1 = address1.replace(/" "/g, "+");
-    const formattedAddress2 = address2.replace(/" "/g, "+");
-    return axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=4707+eilers+avenue+austin+texas&destination=Universal+Studios+Hollywood&key=${googleApi}`, 
-    {
-      "headers": {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-      }});
-  }
-}
-
 function signUserIn(name, email, phonenumber, address, businessid) {
   if (email === undefined && name === undefined) {
     console.log('made call to server which added to database');
   } else {
-    axios.get('/home').then((resolve) => {
-      
+    axios.get('/home').then((resolve) => { 
     });
     console.log(name, email, phonenumber, address, businessid);
   }
@@ -88,4 +72,4 @@ module.exports.signUserIn = signUserIn;
 module.exports.addNewBusiness = addNewBusiness;
 module.exports.findVotingLocations = findVotingLocations;
 module.exports.getCoordinates = getCoordinates;
-module.exports.getDriveTime = getDriveTime;
+module.exports.checkUser = checkUser;
