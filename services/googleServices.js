8bd2f68314c34baf2796db3db06a65fc40240047
Roger Journee <rogerjournee@gmail.com>
2018-09-15 19:41:48 -0500

    copied get coordinates function from front end

--- services/googleServices.js
+++ services/googleServices.js
@@ -1,1 +1,10 @@
 const axios = require('axios');
+
+const getCoordinates = (address) => {
+  if (address === undefined) {
+    console.log('made call to server which added to database');
+  } else {
+    const formattedAddress = address.replace(/" "/g, '+');
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${formattedAddress}&key=${googleApi}`)
+  }
+};
