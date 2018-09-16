c8ad00e1251243bdc497f53f33774a53f61dfef7
JoeyDelahoussaye <jldela@gmail.com>
2018-09-15 19:33:23 -0500

    background

--- dist/Profile.3ec1de09.js
+++ dist/Profile.3ec1de09.js
@@ -5736,7 +5736,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '55786' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49430' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
