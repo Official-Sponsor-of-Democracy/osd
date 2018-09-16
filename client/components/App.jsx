955918c6d5c6f8189a7f0863fba9218bae5eb911
JoeyDelahoussaye <jldela@gmail.com>
2018-09-16 11:45:08 -0500

    added icon on signup complete

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -48,7 +48,7 @@ class App extends React.Component {
     }  
     if (this.state.renderThis === 'businessForm') {
       return (
-        <div className="container-fluid">
+        <div id="business-form" className="container-fluid">
           <BusinessForm changePage={this.changePage.bind(this)} />
         </div>
       );
