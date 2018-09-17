f45df64a3ef4219d3bb69f2f4bc90c7b09560611
Roger Journee <rogerjournee@gmail.com>
2018-09-17 11:24:44 -0500

    Added endpoints and finfished mailjet call

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { saveBusiness } = require('../db/database.js');
+const googleServices = require('../services/googleServices.js');
+const db = require('../db/database.js');
 const { validateBusiness } = require('../db/database.js');
 const { getRefNum } = require('../services/getReferneceNum.js');
-// const { getCoordinates } = require('../services/googleServices.js');
+const { businessSignUpEmail } = require('../services/mailService.js');
+
+const { saveEmployee } = db;
+const { saveBusiness } = db;
+const { getCoordinates } = googleServices;
+const { getPollingLocations } = googleServices;
 
 const app = express();
 
@@ -19,14 +25,17 @@ app.post('/createBusiness', (req, res) => {
   const business = req.body;
   business.referenceNum = getRefNum();
   business.employeeVoterCount = 0;
-
-  // with the info save the given data to the mongo database
-  saveBusiness(business, (newPartner) => {
-    // respond with the saved partner
-    console.log(newPartner);
-    // call the email function
-    res.send(201);
-
+  // get business coordinates to save to the database
+  getCoordinates({ address: '4244 Maple Leaf Dr. New Orleans, LA' }, (latLngObj) => {
+    const latLngString = `${latLngObj.lat},${latLngObj.lng}`;
+    business.coordinates = latLngString;
+    // with the info save the given data to the mongo database
+    saveBusiness(business, (newPartner) => {
+      // console.log(newPartner);
+      // call the email function
+      businessSignUpEmail(business);
+      res.sendStatus(201, 'business saved');
+    });
   });
 });
 
@@ -41,10 +50,47 @@ app.post('/loginBusiness', (req, res) => {
   });
 });
 
+//
+// post /userInfo should take user info from request
 app.post('/userInfo', (req, res) => {
-  const address = req.body;
-  res.end(getCoordinates(address));
+  const userInfo = req.body;
+  // take user address 
+  // make call to get coordinates 
+  getCoordinates(userInfo, (latLngObj) => {
+    // use coordinates to make call to get polling locations function (pass 20 miles in meters also);
+    userInfo.coordinates = `${latLngObj.lat},${latLngObj.lng}`;
+    getPollingLocations(`${latLngObj.lat},${latLngObj.lng}`, (libraries) => {
+      const info = {
+        locations: libraries,
+        userCoordinates: latLngObj,
+      };
+      // save user and home coordinates to database
+      saveEmployee(userInfo, () => {
+        // respond with three closest library locations (should contain address, coordinates, name, user home coordinates)
+        console.log(info)
+        res.send(info);
+      })
+    });
+  });
 });
+
+// take the coordinates of polling location, email
+app.post('/driveTime', (req, res) => {
+
+});
+// find user in database and get their home coordinates & the business id
+// find business with id in database & get there coordinates and get there employerContact and their email
+// make call to google drive time
+// respond with drivetime and employerContact and employer email
+
+// takes the composed email text, employer email, & employee email
+// sends an email to the employee, and the employer of the with the text
+// adds a tally to the business votercount
+
+// app.post('/userInfo', (req, res) => {
+//   const address = req.body;
+//   res.end(getCoordinates(address));
+// });
 
 app.post('/sendEmail', (req, res) => {
   // make a file that exports a stock message with personalized information.
--- services/mailService.js
+++ services/mailService.js
@@ -1,33 +1,37 @@
 const mailJet = require('node-mailjet');
-const { MJ_APIKEY_PRIVATE } = process.env || require('../config.js');
-const { MJ_APIKEY_PUBLIC } = process.env || require('../config.js');
+const { MJ_APIKEY_PRIVATE } = require('../config.js');
+const { MJ_APIKEY_PUBLIC } = require('../config.js');
 
-mailJet
-  .connect(MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE);
+function businessSignUpEmail({ employerContact, referenceNum }) {
+  const request = mailJet
+    .connect(MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE);
 
-mailJet
-  .post('send', {
-    'version': 'v3.1'
-  })
-  .request({
-    Messages: [{
-      From: {
-        Email: 'pilot@mailjet.com',
-        Name: 'Mailjet Pilot',
-      },
-      To: [{
-        Email: 'passenger1@mailjet.com',
-        Name: 'passenger 1',
+  request
+    .post('send', {
+      version: 'v3.1',
+    })
+    .request({
+      Messages: [{
+        From: {
+          Email: 'rogerjournee@gmail.com',
+          Name: 'Boss',
+        },
+        To: [{
+          Email: 'jldela@gmail.com',
+          Name: 'Roger',
+        }],
+        Subject: 'Your email flight plan!',
+        TextPart: `Dear ${employerContact}, Thanks for becoming a partner of ours! Here is your official business reference number for your employees.`,
+        HTMLPart: '<h3>Dear passenger 1, welcome to Mailjet!</h3><br />May the delivery force be with you!',
       }],
-      Subject: 'Your email flight plan!',
-      TextPart: 'Dear passenger 1, welcome to Mailjet! May the delivery force be with you!',
-      HTMLPart: '<h3>Dear passenger 1, welcome to Mailjet!</h3><br />May the delivery force be with you!',
-    }],
-  })
-  .then((result) => {
-    console.log(result.body)
-  })
-  .catch((err) => {
-    console.log(err.statusCode)
-  });
-  
+    })
+    .then((result) => {
+      console.log(result.body);
+      console.log(result.body.Messages[0].To);
+    })
+    .catch((err) => {
+      console.log(err.statusCode);
+    });
+}
+
+module.exports.businessSignUpEmail = businessSignUpEmail;
